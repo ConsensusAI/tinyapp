14605aacba0b129d1d0d20fc12b94e819a946967
ConsensusAI <malik.iqbal@hotmail.ca>
2022-02-19 00:44:36 +0000

    Add /urls.json path that displays a JSON containing the urlDatabase object

--- express_server.js
+++ express_server.js
@@ -10,6 +10,10 @@ app.get("/", (req, res) => {
   res.send("Hello");
 });
 
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
 app.listen(PORT, () => {
   console.log((`Example app listening on port ${PORT}!`));
 });
