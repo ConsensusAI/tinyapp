fb9cc6ecc31f3709c62ae4657a0715bc830288d1
ConsensusAI <malik.iqbal@hotmail.ca>
2022-02-20 17:36:49 +0000

    Fix bug in username submit button and longURL display

--- express_server.js
+++ express_server.js
@@ -2,9 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs") // set ejs as the view engine
+
+app.use(cookieParser());
 
 let generateRandomString = () => {
   let randomID = "";
@@ -30,16 +33,20 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  templateVars = {urls: urlDatabase};
+  const templateVars = {urls: urlDatabase,
+    username: req.cookies["username"]};
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {username: req.cookies["username"]};
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  const templateVars = {shortURL: req.params.shortURL,
+     longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"]};
   res.render("urls_show", templateVars);
 });
 
@@ -61,7 +68,10 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect("/urls");
 });
 
-app.post("/login")
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body["username"]);
+  res.redirect("/urls");
+});
 
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
