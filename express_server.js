c6db039c8bcc80ef9ca1faf46b0919c317fd17fe
ConsensusAI <malik.iqbal@hotmail.ca>
2022-02-20 00:01:43 +0000

    Add POST route to receive form submission

--- express_server.js
+++ express_server.js
@@ -6,6 +6,10 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs") // set ejs as the view engine
 
+function generateRandomString() {
+  
+}
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
@@ -32,6 +36,11 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+})
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
