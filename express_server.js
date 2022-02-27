c2ca0910da27a71f300a6ea5c31e3047789c5d4d
ConsensusAI <malik.iqbal@hotmail.ca>
2022-02-27 00:23:25 +0000

    Improve code readability

--- express_server.js
+++ express_server.js
@@ -39,29 +39,29 @@ app.use(
 const urlDatabase = {};
 const users = {};
 
+// Home, redirects to URLs
 app.get("/", (req, res) => {
   res.redirect("/urls");
 });
 
+// JSON format of URLs
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+// URLs Home Page
 app.get("/urls", (req, res) => {
   const templateVars = {
     urls: urlDatabase,
     user: users[req.session.user_id],
   };
-  console.log("users " + JSON.stringify(users));
-  // console.log("user " + JSON.stringify(users[req.cookies["user_id"]["id"]]));
-  console.log(JSON.stringify(urlDatabase));
-  console.log("user_id " + req.session.user_id);
-  console.log("------------------------");
   res.render("urls_index", templateVars);
 });
 
+// Create new URL page
 app.get("/urls/new", (req, res) => {
   if (!users[req.session.user_id]) {
+    // Redirect to login if user is not logged in
     res.redirect("/login");
   } else {
     const templateVars = {
@@ -71,8 +71,10 @@ app.get("/urls/new", (req, res) => {
   }
 });
 
+// Page for shortened URL
 app.get("/urls/:shortURL", (req, res) => {
   if (!urlDatabase[req.params.shortURL]) {
+    // Redirect to login if user is not logged in
     res.redirect("/urls/new");
   } else {
     const templateVars = {
@@ -84,6 +86,7 @@ app.get("/urls/:shortURL", (req, res) => {
   }
 });
 
+// Login Page
 app.get("/login", (req, res) => {
   const templateVars = {
     user: users[req.session.user_id],
@@ -91,6 +94,7 @@ app.get("/login", (req, res) => {
   res.render("login", templateVars);
 });
 
+// POST new shortened URL
 app.post("/urls", (req, res) => {
   console.log(req.body);
   let newID = generateRandomString(6);
@@ -98,19 +102,20 @@ app.post("/urls", (req, res) => {
     longURL: req.body["longURL"],
     user_id: req.session.user_id,
   };
-  res.redirect(301, `/urls/${newID}`);
+  res.redirect(301, `/urls/${newID}`); // Redirect to its own page
 });
 
+// Used for updating a longURL associated with a shortURL
 app.post("/urls/:shortURL", (req, res) => {
   if (!users[req.session.user_id]) {
     res.redirect("/login");
   } else {
     urlDatabase[req.params.shortURL]["longURL"] = req.body["newlongURL"];
-    console.log(req.body);
     res.redirect(301, `/urls/${req.params.shortURL}`);
   }
 });
 
+// Used to delete shortURL
 app.post("/urls/:shortURL/delete", (req, res) => {
   if (!users[req.session.user_id]) {
     res.redirect("/login");
@@ -122,28 +127,34 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   }
 });
 
+// Used to log in
 app.post("/login", (req, res) => {
+  // Check if email exists in database
   if (compareEmailExistence(req.body["email"], users)) {
     let user = getUserByEmail(req.body["email"], users);
+    // Check if passwords match
     if (bcrypt.compareSync(req.body["password"], users[user]["password"])) {
       req.session.user_id = users[user]["id"];
       res.redirect("/urls");
     } else {
+      // Redirect to urls if password don't match
       res.status(403);
       res.redirect("/urls");
     }
   } else {
+    // Redirect to urls if email doesn't exist
     res.status(403);
     res.redirect("/urls");
   }
-  // res.redirect("/urls");
 });
 
+// Logout Page
 app.post("/logout", (req, res) => {
-  req.session = null;
+  req.session = null; // Destroys the session
   res.redirect("/urls");
 });
 
+// Register Page
 app.get("/register", (req, res) => {
   const templateVars = {
     user: users[req.session.user_id],
@@ -151,11 +162,9 @@ app.get("/register", (req, res) => {
   res.render("register", templateVars);
 });
 
+// POST new user info
 app.post("/register", (req, res) => {
   let checkEmail = req.body["email"];
-  // console.log("check email " + checkEmail);
-  // console.log("post users ", users);
-  // console.log("compare", compareEmailExistence(checkEmail));
   let checkPass = bcrypt.hashSync(req.body["password"], saltRounds);
   if (
     typeof checkEmail !== "string" ||
@@ -180,18 +189,20 @@ app.post("/register", (req, res) => {
     console.log("in register: " + JSON.stringify(users[userID]));
     res.redirect("/urls");
   }
-  console.log(users);
 });
 
+// Redirects to longURL from shortURL
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL]["longURL"];
   res.redirect(longURL);
 });
 
+// Easter Egg
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+// Confirmation of Listening
 app.listen(PORT, () => {
   console.log(`TinyApp listening on port ${PORT}!`);
 });
